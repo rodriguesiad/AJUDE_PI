c4ec037d31a516ff88b944dec06ac01531ca9883
rodriguesiad <irodriguesmoraes79@gmail.com>
2023-11-04 13:23:46 -0300

    Cria modelo base através da listagem de beneficiário

--- src/app/beneficiario/beneficiario.module.ts
+++ src/app/beneficiario/beneficiario.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { BeneficiarioRoutingModule } from './beneficiario-routing.module';
 import { BeneficiarioListComponent } from './components/beneficiario-list/beneficiario-list.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -11,7 +22,19 @@ import { BeneficiarioListComponent } from './components/beneficiario-list/benefi
   ],
   imports: [
     CommonModule,
-    BeneficiarioRoutingModule
+    BeneficiarioRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   exports: [
     BeneficiarioListComponent
