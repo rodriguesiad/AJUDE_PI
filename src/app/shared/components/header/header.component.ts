b151e7c0f873ace52f2d08aec27813e74a85f159
rodriguesiad <irodriguesmoraes79@gmail.com>
2023-11-12 19:26:52 -0300

    Corrige lógica de seleção de navegação do menu principal

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { TabSelectionService } from '../../services/tab-selection.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   navLinks = [
-    {path: '/beneficiarios/list', label: 'BENEFICIÁRIOS'},
-    {path: '/orgaos/list', label: 'ÓRGÃOS'},
-    {path: '/usuarios/list', label: 'USUÁRIOS'}
+    { path: '/beneficiarios/list', label: 'BENEFICIÁRIOS', isActive: false },
+    { path: '/orgaos/list', label: 'ÓRGÃOS', isActive: false },
+    { path: '/usuarios/list', label: 'USUÁRIOS', isActive: false }
   ]
 
-
   constructor(private tabSelectionService: TabSelectionService, private router: Router) { }
+
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+      )
+      .subscribe(event => {
+        this.highlightActiveLink(event.url);
+      });
+  }
+
+  private highlightActiveLink(url: string): void {
+    const linkPrefixes = ['/beneficiarios/', '/orgaos/', '/usuarios/'];
+
+    const foundLink = this.navLinks.find(link => linkPrefixes.some(prefix => url.includes(prefix)) && url.includes(link.path));
+
+    if (foundLink) {
+      this.navLinks.forEach(link => (link.isActive = false));
+      foundLink.isActive = true;
+    }
+  }
 
   navigateToBeneficiariosList() {
     this.router.navigate(['/beneficiarios/list']).then(() => {
